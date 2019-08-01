3f329cd483e0f9bf8ab8f48c5f027961e45cb9b9
Marlon Avery <team@rekure.com>
2019-08-01 15:31:02 -0400

    completed 2D Array

--- array/2dArray.js
+++ array/2dArray.js
@@ -26,6 +26,16 @@ function readLine() {
 
 // Complete the hourglassSum function below.
 function hourglassSum(arr) {
+    let sums = [];
+    var rows = arr.length;
+    var columns = arr[0].length;
+
+    for (var i = 0; i < rows - 2; i++) {
+        for (var j = 0; j < columns - 2; j++) {
+            sums.push(arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+        }
+    }
+    return Math.max(...sums);
 
 
 }
