e94134102442efa2729b23782fb83e73446573dd
Marlon Avery <team@rekure.com>
2019-08-08 11:38:54 -0400

    another solution for abbr

--- bonus/abbr.js
+++ bonus/abbr.js
@@ -1,10 +1,25 @@
-abbrTweet = function (str) {
-    // var Acronyms = {};
-    var splitString = str.trim().split(" ")
-    if (splitString.length > 1) {
-        return (splitString[0].charAt(0) + splitString[1].charAt(0) + splitString[2].charAt(0) +  " " + splitString[3])
-    }
-    return splitString[1];
-}
+// abbrTweet = function (str) {
+//     // var Acronyms = {};
+//     var splitString = str.trim().split(" ")
+//     if (splitString.length > 1) {
+//         return (splitString[0].charAt(0) + splitString[1].charAt(0) + splitString[2].charAt(0) +  " " + splitString[3])
+//     }
+//     return splitString[1];
+// }
 
-console.log(abbrTweet("Be Right Back Mom"))
+// console.log(abbrTweet("Be Right Back Mom"))
+
+// var abbrev = 'Be Right Back'.match(/\b([A-Z])/g).join('');
+// console.log(abbrev)
+
+// var find = /[a-z]{4,}/gi;
+
+// function replace(match) {
+//     return match[0] + (match.length -2 ) + match[match.length - 1];
+// }
+
+// function abbr(string) {
+//     return string.replace(find, replace);
+// }
+
+// console.log(abbr("elephant ride"))
